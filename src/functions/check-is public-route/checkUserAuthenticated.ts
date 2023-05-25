ab1cb0ce8218d3ccfe9a028720cfdfb73226b929
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2023-05-25 17:05:20 -0300

    update

--- src/functions/check-is public-route/checkUserAuthenticated.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import { useSession } from "next-auth/react";
-
-
-export const checkUserAuthenticated = () => {
-  const { data: session, status } = useSession();
-  return !!session && status
-};
